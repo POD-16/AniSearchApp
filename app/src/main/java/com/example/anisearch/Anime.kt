57832345cbad654ab07ec834a1bc40678049a857
amunoz124 <and.munoz41@gmail.com>
2023-04-25 20:51:08 -0700

    Created Anime.kt

---
+++ app/src/main/java/com/example/anisearch/Anime.kt
@@ -0,0 +1,10 @@
+package com.example.anisearch
+
+class Anime(
+    val imageURL: String,
+    val animeTitle: String,
+    val animeRank: Int,
+    val animeStatus: String,
+    val animeSynopsis: String
+) {
+}
